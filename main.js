6af037a638d4acd10005844ba29d7613e0f7f888
Laasya2009 <83776563+Laasya2009@users.noreply.github.com>
2022-08-31 20:42:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+a=10 ; 
+b = 20 ;
+c = a+b ; 
+console.log("Addition is" , c) ; 
+
+c = a*b ; 
+console.log("Multiplication is " , c ) ;
+
+c = b-a ; 
+console.log("Subtraction is ", c ) ; 
+
+c = b/a ; 
+console.log("Division is ", c )
+
+var x =  "Laasya" ; 
+var y = "Karumanchi" ; 
+
+var z = x.concat(y) ; 
+
+console.log(z) ; 
